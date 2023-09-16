7534579c7283fab5b5e46c49fba8cab680ae5ee9
Parth Sanghavi <94062439+parthsanghavi017@users.noreply.github.com>
2023-09-16 09:53:32 +0530

    Update prepro_app.py

--- prepro_app.py
+++ prepro_app.py
@@ -1,3 +1,22 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from chembl_webresource_client.new_client import new_client
+
+# Define a Streamlit app function
+def main():
+    st.title("Data Preprocessing App")
+    st.write("Welcome to the Data Preprocessing App for QSAR analysis.")
+    st.write("This app allows you to preprocess data for QSAR analysis based on a ChemBL ID.")
+    st.write("Please enter your ChemBL ID below to get started.")
+
+    # User input for ChemBL ID
+    chembl_id = st.text_input("Enter ChemBL ID:")
+
+    if st.button("Preprocess Data"):
+        # Preprocess data based on user input
+        preprocess_data(chembl_id)
+
 # Define the preprocess_data function
 def preprocess_data(chembl_id):
     try:
@@ -10,38 +29,35 @@ def preprocess_data(chembl_id):
 
         # Activity search and filtering
         activity = new_client.activity
-        res = activity.filter(target_chembl_id=selected_target).filter(standard_type="IC50").filter(unit="nM")
+        res = activity.filter(target_chembl_id=selected_target, standard_type="IC50", standard_relation="=")
 
-        # Create a directory to store CSV files (if needed)
+        # Create a directory to store CSV files
         output_dir = f"preprocessed_data_{chembl_id}"
-        # os.makedirs(output_dir, exist_ok=True)  # Commented out to provide direct download links
-
+        
         # Step 1: Save the raw data to 'Raw_Data.csv'
         raw_data = pd.DataFrame.from_dict(res)
-        st.write("1. **Raw_Data.csv**: Contains the original data retrieved from ChEMBL.")
-        st.markdown(get_table_download_link(raw_data, f"Raw_Data.csv"), unsafe_allow_html=True)
+        raw_data.to_csv(f"{output_dir}/Raw_Data.csv", index=False)
 
-        # Step 2: Apply filters and save to 'Filtered_Data.csv'
-        filtered_data = raw_data.copy()
-        filtered_data = filtered_data[filtered_data['standard_relation'] == '=']
+        st.write("1. **Raw_Data.csv**: Contains the original data retrieved from ChEMBL.")
+        
+        # Step 2: Filter data and save to 'Filtered_Data.csv'
+        filtered_data = raw_data[['molecule_chembl_id', 'canonical_smiles', 'standard_value']]
         filtered_data = filtered_data.dropna(subset=['standard_value'])
         filtered_data = filtered_data.drop_duplicates(subset=['canonical_smiles', 'molecule_chembl_id'])
-
         filtered_data.to_csv(f"{output_dir}/Filtered_Data.csv", index=False)
-        st.write("2. **Filtered_Data.csv**: Contains data after applying filters for standard type as IC50, standard relation as '=', and removing duplicates.")
-        st.markdown(get_table_download_link(filtered_data, f"Filtered_Data.csv"), unsafe_allow_html=True)
 
-        # Step 3: Get only 3 columns from the filtered_data
-        filtered_data = filtered_data[['molecule_chembl_id', 'canonical_smiles', 'standard_value']].copy()
+        st.write("2. **Filtered_Data.csv**: Contains data after applying filters for standard type as IC50, standard relation as '=', and removing empty standard values and duplicates.")
+        
+        # Step 3: Calculate pIC50 and save to 'Preprocessed_Data.csv'
+        filtered_data['pIC50'] = -np.log10(filtered_data['standard_value'].astype(float) * 1e-9)
+        filtered_data.to_csv(f"{output_dir}/{chembl_id}_Preprocessed_Data.csv", index=False)
 
-        # Add a column to convert standard_value to pIC50 values
-        filtered_data['pIC50'] = -1 * (filtered_data['standard_value'].apply(lambda x: pd.np.log10(x / 1e9)))
+        st.write("3. **Preprocessed_Data.csv**: Contains the final preprocessed data with selected columns including calculated pIC50 values.")
 
-        # Save the preprocessed data to 'Preprocessed_Data.csv'
-        st.write("3. **Preprocessed_Data.csv**: Contains the final preprocessed data with selected columns.")
-        st.markdown(get_table_download_link(filtered_data, f"Preprocessed_Data.csv"), unsafe_allow_html=True)
-
-        st.success("Data preprocessing completed.")
+        # Provide download links to individual CSV files
+        st.markdown(f"**Download Raw Data:** [{chembl_id}_Raw_Data.csv](/{output_dir}/Raw_Data.csv)")
+        st.markdown(f"**Download Filtered Data:** [{chembl_id}_Filtered_Data.csv](/{output_dir}/Filtered_Data.csv)")
+        st.markdown(f"**Download Preprocessed Data:** [{chembl_id}_Preprocessed_Data.csv](/{output_dir}/{chembl_id}_Preprocessed_Data.csv)")
 
         st.write("\n---\n")
         st.write("Powered by Parth Sanghavi")
@@ -52,15 +68,6 @@ def preprocess_data(chembl_id):
     except Exception as e:
         st.error(f"An error occurred: {str(e)}")
 
-
-# Function to generate a download link for a DataFrame
-def get_table_download_link(df, filename):
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">Download {filename}</a>'
-    return href
-
 # Run the Streamlit app
 if __name__ == "__main__":
     main()
-
