eef30b12229872e97ff15d9b6c977a93bf5a48eb
Parth Sanghavi <94062439+parthsanghavi017@users.noreply.github.com>
2023-09-16 09:55:28 +0530

    Update prepro_app.py

--- prepro_app.py
+++ prepro_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from chembl_webresource_client.new_client import new_client
+import base64
 
 # Define a Streamlit app function
 def main():
@@ -31,33 +32,30 @@ def preprocess_data(chembl_id):
         activity = new_client.activity
         res = activity.filter(target_chembl_id=selected_target, standard_type="IC50", standard_relation="=")
 
-        # Create a directory to store CSV files
-        output_dir = f"preprocessed_data_{chembl_id}"
-        
         # Step 1: Save the raw data to 'Raw_Data.csv'
         raw_data = pd.DataFrame.from_dict(res)
-        raw_data.to_csv(f"{output_dir}/Raw_Data.csv", index=False)
+        raw_data.to_csv("Raw_Data.csv", index=False)
 
         st.write("1. **Raw_Data.csv**: Contains the original data retrieved from ChEMBL.")
-        
+
         # Step 2: Filter data and save to 'Filtered_Data.csv'
         filtered_data = raw_data[['molecule_chembl_id', 'canonical_smiles', 'standard_value']]
         filtered_data = filtered_data.dropna(subset=['standard_value'])
         filtered_data = filtered_data.drop_duplicates(subset=['canonical_smiles', 'molecule_chembl_id'])
-        filtered_data.to_csv(f"{output_dir}/Filtered_Data.csv", index=False)
+        filtered_data.to_csv("Filtered_Data.csv", index=False)
 
         st.write("2. **Filtered_Data.csv**: Contains data after applying filters for standard type as IC50, standard relation as '=', and removing empty standard values and duplicates.")
-        
+
         # Step 3: Calculate pIC50 and save to 'Preprocessed_Data.csv'
         filtered_data['pIC50'] = -np.log10(filtered_data['standard_value'].astype(float) * 1e-9)
-        filtered_data.to_csv(f"{output_dir}/{chembl_id}_Preprocessed_Data.csv", index=False)
+        filtered_data.to_csv("Preprocessed_Data.csv", index=False)
 
         st.write("3. **Preprocessed_Data.csv**: Contains the final preprocessed data with selected columns including calculated pIC50 values.")
 
         # Provide download links to individual CSV files
-        st.markdown(f"**Download Raw Data:** [{chembl_id}_Raw_Data.csv](/{output_dir}/Raw_Data.csv)")
-        st.markdown(f"**Download Filtered Data:** [{chembl_id}_Filtered_Data.csv](/{output_dir}/Filtered_Data.csv)")
-        st.markdown(f"**Download Preprocessed Data:** [{chembl_id}_Preprocessed_Data.csv](/{output_dir}/{chembl_id}_Preprocessed_Data.csv)")
+        st.markdown(get_table_download_link(raw_data, "Raw_Data.csv"), unsafe_allow_html=True)
+        st.markdown(get_table_download_link(filtered_data, "Filtered_Data.csv"), unsafe_allow_html=True)
+        st.markdown(get_table_download_link(filtered_data, "Preprocessed_Data.csv"), unsafe_allow_html=True)
 
         st.write("\n---\n")
         st.write("Powered by Parth Sanghavi")
@@ -67,6 +65,13 @@ def preprocess_data(chembl_id):
 
     except Exception as e:
         st.error(f"An error occurred: {str(e)}")
+
+# Function to create a download link for a DataFrame
+def get_table_download_link(df, filename):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">Download {filename}</a>'
+    return href
 
 # Run the Streamlit app
 if __name__ == "__main__":
