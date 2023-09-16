9ef1289153d0880dfa4aeaace99e9b468a375d2e
Parth Sanghavi <94062439+parthsanghavi017@users.noreply.github.com>
2023-09-16 09:40:03 +0530

    Update prepro_app.py

--- prepro_app.py
+++ prepro_app.py
@@ -31,17 +31,21 @@ def preprocess_data(chembl_id):
         activity = new_client.activity
         res = activity.filter(target_chembl_id=selected_target).filter(standard_type="IC50").filter(unit="nM")
 
+        # Create a directory to store CSV files (if needed)
+        output_dir = f"preprocessed_data_{chembl_id}"
+        # os.makedirs(output_dir, exist_ok=True)  # Commented out to provide direct download links
+
         # Step 1: Save the raw data to 'Raw_Data.csv'
         raw_data = pd.DataFrame.from_dict(res)
         st.write("1. **Raw_Data.csv**: Contains the original data retrieved from ChEMBL.")
         st.markdown(get_table_download_link(raw_data, f"{chembl_id}_Raw_Data.csv"), unsafe_allow_html=True)
 
-        # Step 2: Perform filtering and save to 'Filtered_Data.csv' (you can add this step)
+        # Step 2: Apply filters and save to 'Filtered_Data.csv' (add your code here)
         filtered_data = raw_data  # Replace with your filtered data
         st.write("2. **Filtered_Data.csv**: Contains data after applying filters.")
         st.markdown(get_table_download_link(filtered_data, f"{chembl_id}_Filtered_Data.csv"), unsafe_allow_html=True)
 
-        # Step 3: Perform additional preprocessing and save to 'Preprocessed_Data.csv' (you can add this step)
+        # Step 3: Perform additional preprocessing and save to 'Preprocessed_Data.csv' (add your code here)
         preprocessed_data = raw_data  # Replace with your preprocessed data
         st.write("3. **Preprocessed_Data.csv**: Contains the final preprocessed data with selected columns.")
         st.markdown(get_table_download_link(preprocessed_data, f"{chembl_id}_Preprocessed_Data.csv"), unsafe_allow_html=True)
